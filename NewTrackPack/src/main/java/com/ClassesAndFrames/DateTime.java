b7d320cca28d2b445a93ca67469176fad1f0f5c3
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-08-23 14:01:43 +0300

    Create DateTime.java

---
+++ NewTrackPack/src/main/java/com/ClassesAndFrames/DateTime.java
@@ -0,0 +1,16 @@
+package com.ClassesAndFrames;
+
+import java.util.GregorianCalendar;
+
+public class DateTime {
+    public DateTime() {
+    }
+
+    GregorianCalendar cal = new GregorianCalendar();
+    int month = cal.get(java.util.Calendar.MONTH);
+    int day = cal.get(java.util.Calendar.DAY_OF_MONTH);
+    int year = cal.get(java.util.Calendar.YEAR);
+    public String getDate(){
+        return (day + "/" + (month + 1) + "/" + year);
+    }
+}

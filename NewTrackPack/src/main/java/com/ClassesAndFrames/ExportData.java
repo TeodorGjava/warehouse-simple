818ebedb5794cb306766fd5a971d3d614339d86f
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-08-26 15:10:02 +0300

    Update ExportData.java

--- NewTrackPack/src/main/java/com/ClassesAndFrames/ExportData.java
+++ NewTrackPack/src/main/java/com/ClassesAndFrames/ExportData.java
@@ -1,5 +1,6 @@
 
 package com.ClassesAndFrames;
+
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,6 +9,7 @@ import java.io.IOException;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -15,46 +17,46 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExportData {
-    ExportData(){}
-    FileOutputStream FOS;
-    Row row;
-    Cell cell;
-    Workbook wb;
-    XSSFSheet sheet;
-    public void openFile(String file){
-    try{
-    File path =new File(file);
-    java.awt.Desktop.getDesktop().open(path);
-            }catch(IOException e){}
+    ExportData() {
     }
-    public void export(JTable jt) throws IOException{
-    JFileChooser fileChooser = new JFileChooser();
-    fileChooser.showSaveDialog(jt);
-    File saveFile = fileChooser.getSelectedFile();
-    if(saveFile != null){
-    saveFile=new File(saveFile +".xlsx");
-     wb = new XSSFWorkbook();
-     sheet = (XSSFSheet) wb.createSheet("опаковки");
-    Row rowCol = sheet.createRow(0);
-        for (int i = 0; i < jt.getColumnCount(); i++) {
-             cell = rowCol.createCell(i);
-            cell.setCellValue(jt.getColumnName(i));
-        }
-        for (int j = 0; j < jt.getRowCount(); j++) {
-             row = sheet.createRow(j+1);
-            for (int k = 0; k < jt.getColumnCount(); k++) {
-                 cell = row.createCell(k);
-                if(jt.getValueAt(j, k)!= null){
-                cell.setCellValue(jt.getValueAt(j,k).toString());
+
+    private final JFileChooser fileChooser = new JFileChooser();
+    private final Workbook wb = new XSSFWorkbook();
+
+    public void openFile(String file) throws IOException {
+        File path = new File(file);
+        java.awt.Desktop.getDesktop().open(path);
+    }
+
+    public void export(JTable jTable) throws IOException {
+        fileChooser.showSaveDialog(jTable);
+        File saveFile = fileChooser.getSelectedFile();
+        if (saveFile != null) {
+            saveFile = new File(saveFile + ".xlsx");
+            XSSFSheet sheet = (XSSFSheet) wb.createSheet("опаковки");
+            Row rowCol = sheet.createRow(0);
+            Cell cell;
+            for (int i = 0; i < jTable.getColumnCount(); i++) {
+                cell = rowCol.createCell(i);
+                cell.setCellValue(jTable.getColumnName(i));
+            }
+            for (int j = 0; j < jTable.getRowCount(); j++) {
+                Row row = sheet.createRow(j + 1);
+                for (int k = 0; k < jTable.getColumnCount(); k++) {
+                    cell = row.createCell(k);
+                    if (jTable.getValueAt(j, k) != null) {
+                        cell.setCellValue(jTable.getValueAt(j, k).toString());
+                    }
                 }
             }
+            FileOutputStream FOS = new FileOutputStream(saveFile.toString());
+            wb.write(FOS);
+            wb.close();
+            FOS.close();
+            openFile(saveFile.toString());
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Error!");
         }
-         FOS = new FileOutputStream(saveFile.toString());
-        wb.write(FOS);
-        wb.close();
-        FOS.close();
-        openFile(saveFile.toString());
-        
-    }else{JOptionPane.showMessageDialog(null, "error");}
     }
 }

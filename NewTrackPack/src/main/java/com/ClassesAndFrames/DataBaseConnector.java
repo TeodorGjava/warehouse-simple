f7e6257163b6deefbc2cae7c4ea1939b13a9e594
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-08-29 12:58:00 +0300

    Update DataBaseConnector.java

--- NewTrackPack/src/main/java/com/ClassesAndFrames/DataBaseConnector.java
+++ NewTrackPack/src/main/java/com/ClassesAndFrames/DataBaseConnector.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 
 import java.sql.SQLException;
 
-public class DataBaseConnector extends Connector {
+public class DataBaseConnector extends H2DatabaseConnector {
 
 
     protected DataBaseConnector() throws SQLException {

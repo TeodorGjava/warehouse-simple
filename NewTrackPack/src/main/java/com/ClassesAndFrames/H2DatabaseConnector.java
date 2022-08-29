c005e912742e8a46d6b9b2d02321b92e679d4b49
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-08-29 12:57:53 +0300

    Create H2DatabaseConnector.java

---
+++ NewTrackPack/src/main/java/com/ClassesAndFrames/H2DatabaseConnector.java
@@ -0,0 +1,15 @@
+package com.ClassesAndFrames;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class H2DatabaseConnector {
+    Connection connection;
+
+    protected H2DatabaseConnector() throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:h2:./DB/db;IFEXISTS=TRUE", "test", "test");
+    }
+
+
+}
